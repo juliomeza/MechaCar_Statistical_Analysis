674da261ccdb0595b314b7906db7b06d509a0671
Julio Meza <juliomeza@gmail.com>
2022-04-10 23:11:28 -0400

    1

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,1 +1,12 @@
+# Import Libraries
+library(ggplot2)
+library(dplyr)
+library(tidyr)
 
+# Import MechaCar_mpg.csv
+cars_table <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
+head(cars_table)
+
+# Multiple Linear Regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=cars_table) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=cars_table)) #generate summary statistics
